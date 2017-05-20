221e80f709c9e18eca2f322a68c79611b5af56dc
andrecamilo <andrehcamilo@gmail.com>
2017-05-19 23:26:56 -0300

    first commit

--- RAED/script.R
+++ RAED/script.R
@@ -1,1 +1,22 @@
-(1 + 1) * 1
+installed.packages()
+
+search()
+
+library(foreign)
+
+library(help = foreign)
+
+if (!require("datasets")) { 
+	install.packages("datasets")
+}
+
+library("datasets")
+search()
+
+data(package = "datasets")$results
+
+data(diamonds, package = "datasets")
+
+head(diamonds)
+
+
