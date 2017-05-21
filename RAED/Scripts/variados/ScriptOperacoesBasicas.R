ec82f072a1391ef4d92088dfb89ddf94469d3f0c
andrecamilo <andrehcamilo@gmail.com>
2017-05-21 01:52:56 -0300

    adicionado mais exemplos de codigo

--- RAED/Scripts/variados/ScriptOperacoesBasicas.R
+++ RAED/Scripts/variados/ScriptOperacoesBasicas.R
@@ -1,3 +1,34 @@
+#exemplo 1
+mu <- 50
+stddev <- 1
+N <- 10000
+pop <- rnorm(N, mean = mu, sd = stddev)
+plot(density(pop), main = "Population Density", xlab = "X", ylab = "")
+
+
+#exemplo 2
+n <- 30
+samp.means <- rnorm(N, mean = mu, sd = stddev / sqrt(n))
+lines(density(samp.means))
+
+#Exemplo 3
+mu <- 50
+stddev <- 1
+N <- 10000
+pop <- rnorm(N, mean = mu, sd = stddev)
+
+n <- 30
+samp.means <- rnorm(N, mean = mu, sd = stddev / sqrt(n))
+max.samp.means <- max(density(samp.means)$y)
+
+plot(density(pop), main = "Population Density",
+    ylim = c(0, max.samp.means),
+    xlab = "X", ylab = "")
+lines(density(samp.means))
+
+
+
+#Outros exemplos
 A <- c(1, 2, 3, 4, 5)
 
 B <- c(6,7,8,9,10)
