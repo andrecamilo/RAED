916453901ca009ea933d947ff4e144067beb02e4
andrecamilo <andrehcamilo@gmail.com>
2017-05-21 02:41:06 -0300

    adicionado mais exemplos de codigo

--- RAED/Scripts/variados/ScriptOperacoesBasicas.R
+++ RAED/Scripts/variados/ScriptOperacoesBasicas.R
@@ -30,7 +30,6 @@ lines(density(samp.means))
 
 #Outros exemplos
 A <- c(1, 2, 3, 4, 5)
-
 B <- c(6,7,8,9,10)
 
 # media dos elementos de A
@@ -47,4 +46,26 @@ clima
 
 plot(A, B)
 
-?plot
+? plot
+
+
+#box splot
+x = c(5, 5, 5, 13, 7, 11, 11, 9, 8, 9)
+y = c(11, 8, 4, 5, 9, 5, 10, 5, 4, 10)
+boxplot(x, y) #para plotar no mesmo gráfico (comparação)
+boxplot(x);
+boxplot(y) #para plotar em gráficos diferentes
+
+
+
+# grafico de pizza 
+a <- c(0.12, 0.3, 0.26, 0.16, 0.04, 0.12)
+names(a) <- c("a", "b", "c", "d", "e", "f")
+pie(a, col = c("red", "blue", "green", "gray", "brown", "black"))
+
+
+
+#quartis
+z <- c(5, 2, 6, 9, 10, 13, 15)
+summary(z)q <- c(48, 49,  45, 47, 49, 51, 56, 57, 50, 49)
+quantile(q)
